c6d0f946b956b31d2483b8f98fbeb741ba3d9371
panuwith <boomlivecup@gmail.com>
2024-06-18 19:52:50 +0700

    first commit

--- server/Config/db.js
+++ server/Config/db.js
@@ -1,27 +1,27 @@
-const { Pool } = require("pg");
+const { Client } = require("pg");
 require("dotenv").config();
 
-// const client = new Client({
-//   host: process.env.HOST,
-//   port: process.env.POST,
-//   database: process.env.DATABASE,
-//   user: process.env.USER,
-//   password: process.env.PASSWORD,
-// });
-const poolConfig = {
-  max: 5,
-  min: 2,
-  idleTimeoutMillis: 600000,
-};
+const client = new Client({
+  host: process.env.HOST,
+  port: process.env.POST,
+  database: process.env.DATABASE,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+});
+// const poolConfig = {
+//   max: 5,
+//   min: 2,
+//   idleTimeoutMillis: 600000,
+// };
 
-const Host = process.env.HOST;
-const Port = process.env.POST;
-const Database = process.env.DATABASE;
-const Username = process.env.USER;
-const Password = process.env.PASSWORD;
+// const Host = process.env.HOST;
+// const Port = process.env.POST;
+// const Database = process.env.DATABASE;
+// const Username = process.env.USER;
+// const Password = process.env.PASSWORD;
 
-poolConfig.connectionString = `postgres://${Username}:${Password}@${Host}/${Database}`;
+// poolConfig.connectionString = `postgres://${Username}:${Password}@${Host}/${Database}`;
 
-const client = new Pool(poolConfig);
+// const client = new Pool(poolConfig);
 
 module.exports = client;
