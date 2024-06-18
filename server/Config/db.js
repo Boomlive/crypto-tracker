7ba15ced34d732fa345696cd691aa582e3a0381a
panuwith <boomlivecup@gmail.com>
2024-06-18 19:58:47 +0700

    first commit

--- server/Config/db.js
+++ server/Config/db.js
@@ -8,20 +8,5 @@ const client = new Client({
   user: process.env.USER,
   password: process.env.PASSWORD,
 });
-// const poolConfig = {
-//   max: 5,
-//   min: 2,
-//   idleTimeoutMillis: 600000,
-// };
-
-// const Host = process.env.HOST;
-// const Port = process.env.POST;
-// const Database = process.env.DATABASE;
-// const Username = process.env.USER;
-// const Password = process.env.PASSWORD;
-
-// poolConfig.connectionString = `postgres://${Username}:${Password}@${Host}/${Database}`;
-
-// const client = new Pool(poolConfig);
 
 module.exports = client;
