0600d551514c04dc839072426c6fc677b1b09e33
panuwith <boomlivecup@gmail.com>
2024-06-19 19:40:21 +0700

    first commit

--- server/Config/db.js
+++ server/Config/db.js
@@ -1,15 +1,12 @@
-const { Pool } = require("pg");
+const { Client } = require("pg");
 require("dotenv").config();
 
-const client = new Pool({
+const client = new Client({
   host: process.env.HOST,
   port: process.env.POST,
   database: process.env.DATABASE,
   user: process.env.USER,
   password: process.env.PASSWORD,
-  connectionTimeoutMillis: 5000, // Adjust the timeout value as needed
-  idleTimeoutMillis: 60000, // Timeout after which idle clients are closed
-  max: 20, // Maximum number of clients in the pool
 });
 
 module.exports = client;
